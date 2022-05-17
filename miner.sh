5fdf5d7a93bf7afa7f42cf0d04138cf4bea2fabe
Pravin L <75366950+Pravinlsiye@users.noreply.github.com>
2022-05-17 16:55:10 +0530

    Update and rename new project to miner.sh

---
+++ miner.sh
@@ -0,0 +1,9 @@
+
+:start
+timeout 60
+
+--------------minig config------------------
+ccminer-x64 -a x17 -o stratum+tcp://yiimp.eu:3777 -u DGdpgo3qLwfLFX8KSrocHCNiPgW7RyxJdQ.grig -p c=XVG
+
+--------------------------------------------
+goto start
